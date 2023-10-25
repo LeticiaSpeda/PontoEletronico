0ad939ac2c0bba57b6e922b7da401c4921210d69
Leticia Speda <leticiaspeda@gmail.com>
2023-10-25 08:13:42 -0300

    build: configuracoes iniciais

---
+++ PontoEletronico/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PontoEletronico
+//
+//  Created by Leticia Speda on 25/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
